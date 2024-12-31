5282b07bc55ca1b2e6ca08a34935fd4dc19d3fd5
Ebite God'sgift Uloamaka <godsgiftuloamaka235@gmail.com>
2024-12-31 13:04:57 +0100

    type_switches: question 1 and solution #boot.dev

---
+++ interface_/type_switches/main.go
@@ -0,0 +1,107 @@
+/* Assignment
+After submitting our last snippet of code for review, a more experienced gopher told us to use a type switch instead of successive assertions. Let's make that improvement!
+
+Implement the getExpenseReport function using a type switch.
+
+If the expense is an email then it should return the email's toAddress and the cost of the email.
+If the expense is an sms then it should return the sms's toPhoneNumber and its cost.
+If the expense has any other underlying type, just return an empty string and 0.0 for the cost. */
+// ---------------------------------------------
+/* package main
+
+func getExpenseReport(e expense) (string, float64) {
+	// ?
+}
+
+// don't touch below this line
+
+type expense interface {
+	cost() float64
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
+} */
+//-------------------------solution-------------------------
+package main
+
+func getExpenseReport(e expense) (string, float64) {
+	switch v := e.(type) {
+		case email: 
+			return v.toAddress, v.cost()
+		case sms: 
+			return v.toPhoneNumber, v.cost()
+		default:
+			return "", 0.0	
+	}
+}
+
+// don't touch below this line
+
+type expense interface {
+	cost() float64
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
+}
+
+func main() {
+	runTestCases()
+}
---
+++ interface_/type_switches/test_case.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+)
+
+var withSubmit = true
+
+func runTestCases() {
+	type testCase struct {
+		expense      expense
+		expectedTo   string
+		expectedCost float64
+	}
+	tests := []testCase{
+		{
+			email{true, "hello there", "kit@boga.com"},
+			"kit@boga.com",
+			0.11,
+		},
+		{
+			sms{false, "I'm a Nigerian prince, please send me your bank info so I can deposit $1000 dollars", "+155555509832"},
+			"+155555509832",
+			8.3,
+		},
+	}
+
+	if withSubmit {
+		tests = append(tests, []testCase{
+			{invalid{}, "", 0},
+			{
+				email{false, "This meeting could have been an email", "jane@doe.com"},
+				"jane@doe.com",
+				1.85,
+			},
+			{
+				sms{false, "Please sir/madam", "+155555504444"},
+				"+155555504444",
+				1.6,
+			},
+		}...)
+	}
+
+	passCount := 0
+	failCount := 0
+
+	for _, test := range tests {
+		to, cost := getExpenseReport(test.expense)
+		if to != test.expectedTo || cost != test.expectedCost {
+			failCount++
+			fmt.Printf(`---------------------------------
+Inputs:     %+v
+Expecting:  (%v, %v)
+Actual:     (%v, %v)
+Fail`, test.expense, test.expectedTo, test.expectedCost, to, cost)
+		} else {
+			passCount++
+			fmt.Printf(`---------------------------------
+Inputs:     %+v
+Expecting:  (%v, %v)
+Actual:     (%v, %v)
+Pass
+`, test.expense, test.expectedTo, test.expectedCost, to, cost)
+		}
+	}
+
+	fmt.Println("---------------------------------")
+	fmt.Printf("%d passed, %d failed\n", passCount, failCount)
+}
