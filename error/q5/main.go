af485cfa874e558356a4ff173626f9709a31cbf3
Ebite God'sgift Uloamaka <godsgiftuloamaka235@gmail.com>
2025-01-02 03:11:29 +0100

    error: question 5 and solution #boot.dev

---
+++ error/q5/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "errors"
+
+func validateStatus(status string) error {
+	if status == "" {
+		return errors.New("status cannot be empty")
+	} else if len(status) > 140 {
+		return errors.New("status exceeds 140 characters")
+	}
+	return nil
+}
+
+func main() {
+	runTestCases()
+}
