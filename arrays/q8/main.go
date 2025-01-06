daaa6f74097e60b93a766aa6b8d3a54a7edea18b
Ebite God'sgift Uloamaka <godsgiftuloamaka235@gmail.com>
2025-01-06 04:26:56 +0100

    array: question 8 and solution #boot.dev

---
+++ arrays/q8/main.go
@@ -0,0 +1,50 @@
+package main
+
+type Message interface {
+	Type() string
+}
+
+type TextMessage struct {
+	Sender  string
+	Content string
+}
+
+func (tm TextMessage) Type() string {
+	return "text"
+}
+
+type MediaMessage struct {
+	Sender    string
+	MediaType string
+	Content   string
+}
+
+func (mm MediaMessage) Type() string {
+	return "media"
+}
+
+type LinkMessage struct {
+	Sender  string
+	URL     string
+	Content string
+}
+
+func (lm LinkMessage) Type() string {
+	return "link"
+}
+
+// Don't touch above this line
+
+func filterMessages(messages []Message, filterType string) []Message {
+	var filtered []Message
+	for _, message := range messages {
+		if message.Type() == filterType {
+			filtered = append(filtered, message)
+		}
+	}
+	return filtered
+}
+
+func main() {
+	runTestCases()
+}
