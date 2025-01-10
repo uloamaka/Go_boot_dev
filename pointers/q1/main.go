6d29918e8572d6e59e03918bd30b390f87394075
Ebite God'sgift Uloamaka <godsgiftuloamaka235@gmail.com>
2025-01-10 04:08:03 +0100

    pointers: question 1 and solution #boot.dev

---
+++ pointers/q1/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Message struct {
+	Recipient string
+	Text      string
+}
+
+func getMessageText(m Message) string {
+	return fmt.Sprintf(`
+To: %v
+Message: %v
+`, m.Recipient, m.Text)
+}
+
+
+func main() {
+	runTestCases()
+}
+// algorithm
+// ------------------------------------
