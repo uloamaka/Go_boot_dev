5ae15b488fae1d06a361df027c622063fd76a147
Ebite God'sgift Uloamaka <godsgiftuloamaka235@gmail.com>
2025-01-13 17:29:10 +0100

    enums: question 2 and solution #boot.dev

---
+++ enums/q2/main.go
@@ -0,0 +1,17 @@
+package main
+
+type emailStatus = int
+
+const (
+	emailBounced emailStatus = iota
+	emailInvalid 
+	emailDelivered 
+	emailOpened 
+)
+// algorithm:
+// Define an emailStatus type that uses iota syntax to represent the following states:
+
+// emailBounced: 0
+// emailInvalid: 1
+// emailDelivered: 2
+// emailOpened: 3
